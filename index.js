d0ef52f3254976a3a4291cb95c7a7369e4fa4cc3
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-19 08:28:59 +0300

    Adding JS file

---
+++ index.js
@@ -0,0 +1,67 @@
+const calculator = document.querySelector(".calculator");
+const formulaScreen = document.querySelector(".formula-screen");
+const outputScreen = document.querySelector(".output-screen");
+
+let previousOperand = "";
+let currentOperand = "";
+let mathOperator = undefined;
+
+calculator.addEventListener("click", (e) => {
+    if (e.target.matches("button.button")) {
+      let clickedButton = e.target.value;
+  
+      switch (clickedButton) {
+        case "AC":
+          clear();
+          break;
+        case "%":
+        case "/":
+        case "*":
+        case "-":
+        case "+":
+          appendMathOperator(clickedButton);
+          updateDisplay();
+          break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+          appendNumber(clickedButton);
+          updateDisplay();
+          break;
+        case "=":
+          calculation();
+          updateDisplay();
+          break;
+        default:
+          console.log("Something went wrong!");
+      }
+    }
+  });
+
+  function appendNumber(number) {
+    if (number === "." && currentOperand.includes(".")) return;
+    currentOperand = currentOperand.toString() + number.toString();
+  }
+
+  function updateDisplay() {
+    outputScreen.value = currentOperand;
+    if (mathOperator) {
+      formulaScreen.value = `${parseFloat(previousOperand)} ${mathOperator}`;
+    } else {
+      formulaScreen.value = "";
+    }
+  }
+
+  
+
+  
+
+  
