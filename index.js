14d640dfec47c154cd9dae00b656dd81dccc38d8
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-09-03 13:24:07 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,113 @@
+const calculator = document.querySelector(".calculator");
+const formulaScreen = document.querySelector(".formula-screen");
+const outputScreen = document.querySelector(".output-screen");
+
+let previousOperand = "";
+let currentOperand = "";
+let mathOperator = undefined;
+
+calculator.addEventListener("click", (e) => {
+    if (e.target.matches("button.button")) {
+      let clickedButton = e.target.value;
+  
+      switch (clickedButton) {
+        case "AC":
+          clear();
+          break;
+        case "%":
+        case "/":
+        case "*":
+        case "-":
+        case "+":
+          appendMathOperator(clickedButton);
+          updateDisplay();
+          break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+          appendNumber(clickedButton);
+          updateDisplay();
+          break;
+        case "=":
+          calculation();
+          updateDisplay();
+          break;
+        default:
+          console.log("Something went wrong!");
+      }
+    }
+  });
+
+  function appendNumber(number) {
+    if (number === "." && currentOperand.includes(".")) return;
+    currentOperand = currentOperand.toString() + number.toString();
+  }
+
+  function updateDisplay() {
+    outputScreen.value = currentOperand;
+    if (mathOperator) {
+      formulaScreen.value = `${parseFloat(previousOperand)} ${mathOperator}`;
+    } else {
+      formulaScreen.value = "";
+    }
+  }
+
+  function clear() {
+    formulaScreen.value = "";
+    outputScreen.value = 0;
+    mathOperator = undefined;
+    previousOperand = "";
+    currentOperand = "";
+  }
+
+  function appendMathOperator(operator) {
+    if (currentOperand === "") {
+        return;
+    };
+
+    if (previousOperand !== "") {
+      calculation();
+    }
+
+    if (operator == "%") {
+        currentOperand = currentOperand / 100;
+    } else {
+        mathOperator = operator;
+        previousOperand = currentOperand;
+        currentOperand = "";
+    }
+  }
+
+  function calculation() {
+    let result;
+    const previousValue = parseFloat(previousOperand);
+    const currentValue = parseFloat(currentOperand);
+    switch (mathOperator) {
+      case "/":
+        result = previousValue / currentValue;
+        break;
+      case "*":
+        result = previousValue * currentValue;
+        break;
+      case "-":
+        result = previousValue - currentValue;
+        break;
+      case "+":
+        result = previousValue + currentValue;
+        break;
+      default:
+        return;
+    }
+    
+    currentOperand = result;
+    mathOperator = undefined;
+    previousOperand = "";
+  }  
